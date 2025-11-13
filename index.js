79721122860c3a40dd5bd6e536723d087c26f7cb
Baki-2025 <abz.147570@gmail.com>
2025-11-13 13:55:04 +0600

    4th commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-// ✅ Correctly use env variables
+// MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.psactc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -26,25 +26,42 @@ async function run() {
     await client.connect();
     const db = client.db("learningDB");
     const courseCollection = db.collection("courses");
+    const instructorCollection = db.collection("instructors");
 
-    app.post('/courses',async (req,res)=>{
+    // ---------- Courses Routes ----------
+    app.post('/courses', async (req, res) => {
       const newCourse = req.body;
-      const result = await courseCollection.insertOne(newCourse)
-    })
-    app.delete('/courses/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await coursesCollection.deleteOne(query);
-      res.send(result)
-    })
-
-    app.get("/", (req, res) => {
-      res.send("Learning Server is running");
+      const result = await courseCollection.insertOne(newCourse);
+      res.send(result);
     });
 
-    app.get("/courses", async (req, res) => {
+    app.delete('/courses/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await courseCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get('/courses', async (req, res) => {
       const result = await courseCollection.find().toArray();
       res.send(result);
+    });
+
+    // ---------- Instructors Routes ----------
+    app.post('/instructors', async (req, res) => {
+      const newInstructor = req.body;
+      const result = await instructorCollection.insertOne(newInstructor);
+      res.send(result);
+    });
+
+    app.get('/instructors', async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Root route
+    app.get("/", (req, res) => {
+      res.send("Learning Server is running");
     });
 
     console.log("✅ MongoDB connected successfully!");
